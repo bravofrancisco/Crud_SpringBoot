e5a96ac9d6faa4b960b6bcad6384c1a92be72ed4
Francisco Bravo <fcobravo.23@gmail.com>
2025-07-18 18:08:05 -0400

    agregando Cors

---
+++ src/main/java/com/example/demo_practica02/Cors.java
@@ -0,0 +1,23 @@
+package com.example.demo_practica02;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class Cors {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/**")
+                        .allowedOrigins("http://localhost:5173") // el frontend
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedHeaders("*");
+            }
+        };
+    }
+}
